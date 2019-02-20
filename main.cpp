e86333c96108bd7e7dbaee41fe19703947464216
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2019-02-19 22:26:52 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,28 +2,63 @@
 
 using namespace std;
 
-int feetInchesInput(int& feet, int& inches);
-double feetToMeters(double feet, double meters);
-double inchesToMilimeters(double inches, double milimeters);
-void output(double feet, double, inches, double Meters, double milimeters);
+const double metersInAFoot = 0.3048;
+const double centimetersInAMeter = 100;
+const double inchesInAFoot = 12;
+
+// Its pretty pointless to do centiimeters, the conversion from meters to centimeters is a no brainer...
+
+void feetInchesInput(double& feet, double& inches);
+// void inchesToFeet(double& feet, double& inches, double& feetAdded);
+// void feetToMeters(double feet, double& meters, double& centimeters);
+void conversions(double& feet, double& inches, double& feetAdded, double& meters, double& centimeters);
+void output(double feet, double inches, double meters, double centimeters, double feetAdded);
 
 int main(){
-    int feet;
-    int inches;
+    double feet;
+    double feetAdded;
+    double inches;
     double meters;
-    double milimeters;
+    double centimeters;
 
     feetInchesInput(feet, inches);
-    inchesToMilimeters(feet, inches)
+    // inchesToFeet(feet, inches, feetAdded);
+    // feetToMeters(feet, meters, centimeters);
+    conversions(feet, inches, feetAdded, meters, centimeters);
+    output(feet, inches, meters, centimeters, feetAdded);
 
     return 0;
 }
 
-int feetInchesInput(int& feet, int& inches){
+void feetInchesInput(double& feet, double& inches){
+    
+    cout << "This is Feet/Inches to Meters/Centimeters conversion." << endl << endl;
+
     cout << "How many feet? :  ";
     cin >> feet;
     cout << "How many inches? :  ";
     cin >> inches;
+    cout << endl;
 }
 
-double feetToMeters(int)
+void conversions(double& feet, double& inches, double& feetAdded, double& meters, double& centimeters){
+    feet = feet + (inches / inchesInAFoot);
+    feetAdded = (inches / inchesInAFoot);
+    meters = (feet * metersInAFoot);
+    centimeters = (meters * centimetersInAMeter);
+}
+
+// void inchesToFeet(double& feet, double& inches, double& feetAdded){
+//     feet = feet + (inches / inchesInAFoot);
+//     feetAdded = (inches / inchesInAFoot);
+// }
+
+// void feetToMeters(double feet, double& meters, double& centimeters){
+//     meters = (feet * metersInAFoot);
+//     centimeters = (meters * centimetersInAMeter);
+// }
+
+void output(double feet, double inches, double meters, double centimeters, double feetAdded){
+    cout << (feet - feetAdded) << " feet and " << inches << " inches (" << feetAdded << " more feet) converts to: " << endl;
+    cout << meters << " meters or " << centimeters << " centimeters." << endl;
+}
