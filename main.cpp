a673ac15d352b8a5338e8212a82a719265e7398e
ShinKojiro <42621442+ShinKojiro@users.noreply.github.com>
2019-02-20 19:27:24 -0600

    Fixed redundancy

--- main.cpp
+++ main.cpp
@@ -6,30 +6,6 @@ const double METERS_IN_A_FOOT = 0.3048;
 const double CENTIMETERS_IN_A_METER = 100;
 const double INCHES_IN_A_FOOT = 12;
 
-// Its pretty pointless to do centimeters, the conversion from meters to centimeters is a no brainer...
-
-void feetInchesInput(double& feet, double& inches);
-// void inchesToFeet(double& feet, double& inches, double& feetAdded);
-// void feetToMeters(double feet, double& meters, double& centimeters);
-void conversions(double& feet, double& inches, double& feetAdded, double& meters, double& centimeters);
-void output(double feet, double inches, double meters, double centimeters, double feetAdded);
-
-int main(){
-    double feet;
-    double feetAdded;
-    double inches;
-    double meters;
-    double centimeters;
-
-    feetInchesInput(feet, inches);
-    // inchesToFeet(feet, inches, feetAdded);
-    // feetToMeters(feet, meters, centimeters);
-    conversions(feet, inches, feetAdded, meters, centimeters);
-    output(feet, inches, meters, centimeters, feetAdded);
-
-    return 0;
-}
-
 void feetInchesInput(double& feet, double& inches){
     
     cout << "This is Feet/Inches to Meters/Centimeters conversion." << endl << endl;
@@ -62,3 +38,27 @@ void output(double feet, double inches, double meters, double centimeters, doubl
     cout << (feet - feetAdded) << " feet and " << inches << " inches (" << feetAdded << " more feet) converts to: " << endl;
     cout << meters << " meters or " << centimeters << " centimeters." << endl;
 }
+// Its pretty pointless to do centimeters, the conversion from meters to centimeters is a no brainer...
+
+// void feetInchesInput(double& feet, double& inches);
+// void inchesToFeet(double& feet, double& inches, double& feetAdded);
+// void feetToMeters(double feet, double& meters, double& centimeters);
+// void conversions(double& feet, double& inches, double& feetAdded, double& meters, double& centimeters);
+// void output(double feet, double inches, double meters, double centimeters, double feetAdded);
+
+int main(){
+    double feet;
+    double feetAdded;
+    double inches;
+    double meters;
+    double centimeters;
+
+    feetInchesInput(feet, inches);
+    // inchesToFeet(feet, inches, feetAdded);
+    // feetToMeters(feet, meters, centimeters);
+    conversions(feet, inches, feetAdded, meters, centimeters);
+    output(feet, inches, meters, centimeters, feetAdded);
+
+    return 0;
+}
+
